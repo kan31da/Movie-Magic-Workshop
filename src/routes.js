ea982bf01c57c5c683671d2e0d6587c6a79c2968
kan31da <vladislavvladovski@gmail.com>
2026-07-14 19:07:06 +0300

    add auth controller

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 import artistsController from './controllers/artistsController.js';
+import authController from './controllers/authController.js';
 
 const routes = Router();
 
@@ -12,6 +13,8 @@ routes.use('/', homeController);
 routes.use('/movies', movieController);
 
 routes.use('/artists', artistsController);
+
+routes.use('/auth', authController);
 
 routes.get('*url', (req, res) => {
     res.status(404).render('404', { title: 'Page Not Found' });
